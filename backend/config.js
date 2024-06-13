71bdc2c5b271106e26e72da504c57fcf1b7e1f92
Alvaro Condori <con21033@byui.edu>
2024-06-13 10:25:47 -0300

    Add MongoDB and mongoose to nodejs

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+
+export const mongoDBURL =
+  "mongodb+srv://mongoUser:mongoUser123@books-store-mern.lsvzx8k.mongodb.net/book-collection?retryWrites=true&w=majority&appName=Books-Store-MERN";
